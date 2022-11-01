c1b0f6aec1b6bdd5edea187c32ec01f867fc662d
Pedro Luiz <pedroluizlsg@gmail.com>
2022-11-01 18:47:05 -0300

    Req 8 - finalizado - Pesquisa albums de Elis Regina

---
+++ desafio8.sql
@@ -0,0 +1,7 @@
+SELECT 
+  art.artist AS "artista",
+  alb.album AS "album"   
+FROM SpotifyClone.artists AS art
+INNER JOIN SpotifyClone.albums AS alb
+ON art.artist_id = alb.artist_id
+WHERE art.artist = "Elis Regina";
